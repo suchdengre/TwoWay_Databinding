81c46fcda5ae5a3d9bb16522a8abba8a796a5adb
ADMIN <suchidengreit56@gmail.com>
2024-06-12 11:01:34 +0530

    Twowaybinding

---
+++ app/src/main/java/com/example/twowaybinding/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.twowaybinding
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.twowaybinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        binding.myViewModel = viewModel
+        binding.lifecycleOwner = this
+
+    }
+}
